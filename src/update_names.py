d14612ea9674e4ba36fff5b6b0f738fb26c621e4
Topher McKee <tophermckee@gmail.com>
2022-10-16 15:05:40 -0500

    title case

--- src/update_names.py
+++ src/update_names.py
@@ -33,7 +33,7 @@ def main():
                         {
                             "type": "text",
                             "text": {
-                                "content": f"{item['properties']['Title']['rich_text'][0]['text']['content']} by {item['properties']['Author / Publisher / Brand']['rich_text'][0]['text']['content']}",
+                                "content": f"{item['properties']['Title']['rich_text'][0]['text']['content'].title()} by {item['properties']['Author / Publisher / Brand']['rich_text'][0]['text']['content'].title()}",
                                 "link": None
                             },
                             "annotations": {
@@ -44,7 +44,7 @@ def main():
                                 "code": False,
                                 "color": "default"
                             },
-                            "plain_text": f"{item['properties']['Title']['rich_text'][0]['text']['content']} by {item['properties']['Author / Publisher / Brand']['rich_text'][0]['text']['content']}",
+                            "plain_text": f"{item['properties']['Title']['rich_text'][0]['text']['content'].title()} by {item['properties']['Author / Publisher / Brand']['rich_text'][0]['text']['content'].title()}",
                             "href": None
                         }
                     ]
@@ -53,7 +53,7 @@ def main():
         }
         update_attempt = requests.patch(url=f"https://api.notion.com/v1/pages/{item['id']}", json=payload, headers=headers).json()
         logging.info(update_attempt)
-        print(f"✅ Updated: {item['properties']['Title']['rich_text'][0]['text']['content']} by {item['properties']['Author / Publisher / Brand']['rich_text'][0]['text']['content']}")
+        print(f"✅ Updated: {item['properties']['Title']['rich_text'][0]['text']['content'].title()} by {item['properties']['Author / Publisher / Brand']['rich_text'][0]['text']['content'].title()}")
 
 if __name__ == '__main__':
     main()
